0a2cee7d050fba757d8791a39757374549b97ed4
redkrayn <redkrayn@yandex.ru>
2025-03-29 21:35:11 +0800

    Update project

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'TeamProject2_Flower.settings'
+import django
+django.setup()
 import asyncio
 import logging
-import os
-from aiogram import Bot, Dispatcher, types
-from dotenv import load_dotenv
 
+from aiogram import Bot, Dispatcher, types
 from Handlers.handlers import user_private_router
 from Common.bot_cmds_list import private
+from dotenv import load_dotenv
 
 
 async def main():
     load_dotenv()
     dp = Dispatcher()
     dp.include_routers(user_private_router)
-    BOT_TOKEN = os.getenv('BOT_TG_TOKEN')
+    bot_token = os.getenv('BOT_TG_TOKEN')
     logging.basicConfig(level=logging.INFO)
-    bot = Bot(token=BOT_TOKEN)
+    bot = Bot(token=bot_token)
     await bot.delete_webhook(drop_pending_updates=True)
     await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot)
