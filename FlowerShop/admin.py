65921b8c4666269336f3caf244f4e19a68af5e70
redkrayn <redkrayn@yandex.ru>
2025-04-01 02:08:07 +0800

    Update admin.py

--- FlowerShop/admin.py
+++ FlowerShop/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import *
 
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('flower_name', 'customer', 'order_status', 'delivery_date', 'order_price')
+
+
 admin.site.register(Bouquet)
 admin.site.register(Customer)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
